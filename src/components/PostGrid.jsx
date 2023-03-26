24658d61d6966bf8a8afa8f25cc10613afb79f7a
Joseph Kim <josephkim@Josephs-MacBook-Air.local>
2023-03-26 13:02:12 -0400

    Posts are now sorted by date of creation newest first

--- src/components/PostGrid.jsx
+++ src/components/PostGrid.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { Row, Col} from "react-bootstrap"
 import Post from "../components/Post"
 
+function RenderPost(post) {
+    return (
+        <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Post post={post} /> 
+        </Col>
+    )
+}
+
 function PostGrid(props) {
     const posts = props.posts;
+    posts.sort((a, b) => a.created_at - b.created_at);
     return (
-        <div>
-            <Row className="g-4" lg={1} xs={1}>
-                {posts.map((post) => (
-                    <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Post post={post} /> 
-                    </Col>
-                ))}
-            </Row>
-        </div>
+        <Row className="g-4" lg={1} xs={1}>
+            {posts.map((post) => (RenderPost(post)))}
+        </Row>
     )
 }
 
